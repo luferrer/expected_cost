18d34eb6ccbddd9c8e2caf23d8b52c1acab55fca
Luciana Ferrer <lferrer@dc.uba.ar>
2022-10-24 11:45:09 -0300

    Update data.py
    Better comments.

--- examples/data.py
+++ examples/data.py
@@ -17,19 +17,27 @@ except:
 
 def get_llks_for_multi_classif_task(dataset, priors=None, K=100000):
     """ Load or create multi-class log (potentially scaled) likelihoods (llks).
-    The cifar10 example can be modified to load your own set of scores.
-    If they are posteriors instead of llks, you can either: 
+    The method outputs both the raw (potentially miscalibrated) scores and 
+    calibrated scores.
+         
+    Two datasets are implemented here:
+    * cifar10: pre-generated scores for cifar10 data
+    * gaussian_sim: simulated data with Gaussian distributions. In this case,
+      the priors and K need to be set to determine the class priors and the 
+      total number of samples required.
+      
+    The cifar10 example can be used as template add a loader for your own set 
+    of scores. If your scores are posteriors instead of llks, you can either: 
     * ignore this fact and have this method output the posteriors (in this case,  
       remember to call the expected cost methods with score_type="log-posteriors")
-    * convert them to estimated llks by subtracting log(priors) estimated based
-      on the training data and procedure
-    * calibrate them using psrcal with uniform external priors to get log scaled
-      likelihoods.
+    * convert them to estimated llks by using logpost_to_log_scaled_lks where the 
+      priors can be estimated as those used in training.
+
     """"
 
     if 'cifar10' in dataset:
 
-        # Load some scores from a resnet and targets for cifar10 data
+        # Load pre-generated scores for cifar10 data
         print("\n**** Loading in CIFAR10 data ****\n")
         preacts = np.load("data/resnet-50_cifar10/predictions.npy")
         targets = np.load("data/resnet-50_cifar10/targets.npy")
@@ -53,20 +61,16 @@ def get_llks_for_multi_classif_task(dataset, priors=None, K=100000):
         raw_llks = raw_logpost
         cal_llks = cal_logpost
 
-
     elif dataset == 'gaussian_sim':
 
-        # Ccreate scores using a Gaussian distribution for each class. These scores will not
+        # Create scores using a Gaussian distribution for each class. These scores will not
         # necessarily be well calibrated. 
-        print("\n**** Creating simulated data with Gaussian class distributions and uniform priors ****\n")
-
+        print("\n**** Creating simulated data with Gaussian class distributions ****\n")
         np.random.seed(0)
 
         counts = np.array(np.array(priors)*K, dtype=int)
         C = len(counts)
         
-        # Draw the means from a gaussian distribution centered at 0 with 
-        # diagonal covariance with std=10 for all dimensions
         # We need C means, each of dimension C-1 (since the first 
         # dimension of the simulated scores is always 0, because the
         # scores are meant to be log scaled-likelihoods and the scale
@@ -74,7 +78,6 @@ def get_llks_for_multi_classif_task(dataset, priors=None, K=100000):
         # We set these means to produce scores that tend to be larger
         # for the true class.
         alpha = 1.5
-        std = 0.8
         m = -0.5 * np.ones(C-1)
         means = [m]
         for i in np.arange(1,C):
@@ -82,9 +85,11 @@ def get_llks_for_multi_classif_task(dataset, priors=None, K=100000):
             m[i-1] = alpha
             means.append(m)
 
-        # All classes have identity covariance 
+        # Use the same diagonal covariance matrix for all classes.
+        std = 0.8
         stds   = np.ones([C,C-1]) * std
 
+        # Draw scores from these distributions
         raw_llks, targets = draw_scores_for_gaussian_model(means, stds, counts)
 
         # Now get the LLRs given the model we chose for the distributions
@@ -100,10 +105,24 @@ def get_llks_for_multi_classif_task(dataset, priors=None, K=100000):
 
 
 def get_llrs_for_bin_classif_task(dataset, prior1=None, K=100000):
-    """ Load or simulate data for binary classification. For the simulations,
-    K and P1 are needed, indicating the number if required samples, and the
-    proportion of those samples that should be from class 1."""
-
+    """ Load or create log-likelihood ratios (llrs) for a binary classification 
+    task. The method outputs both the raw (potentially miscalibrated) scores and 
+    calibrated scores.
+         
+    Two datasets are implemented here:
+    * cifar10_first2classes: pre-generated scores for cifar10 data. We simulate 
+      a 2-class problem by selecting just the first two scores.
+    * gaussian_sim: simulated data with Gaussian distributions. In this case,
+      the prior for class 1 and K, the total number of required samples, need 
+      to be set.
+      
+    The cifar10 example can be used as template add a loader for your own set 
+    of scores. If your scores are posteriors of llrs, you can either: 
+    * ignore this fact and have this method output the posteriors (in this case,  
+      remember to call the expected cost methods with score_type="log-posteriors")
+    * convert them to estimated llrs using logpost_to_llrs with priors estimated 
+      based on the training data and procedure
+    """"
 
     if 'cifar10_first2classes' in dataset:
 
@@ -133,18 +152,17 @@ def get_llrs_for_bin_classif_task(dataset, prior1=None, K=100000):
         # data so, to get the LLR, we just need to subtract the  log
         # posterior for class 1 from the log posterior from class 0. If
         # this was not the case, we would use
-        # utils.logpost_to_llrs(logpost, priors), where the priors are
-        # those used in training.
+        # utils.logpost_to_llrs(logpost, priors), where the priors can 
+        # be estimated as those used in training.
         raw_llrs = raw_logpost[:,1] - raw_logpost[:,0]
         cal_llrs = cal_logpost[:,1] - cal_logpost[:,0]
 
 
     elif dataset == 'gaussian_sim':
 
-        # Ccreate scores using a Gaussian distribution for each class. These scores will not
+        # Create scores using a Gaussian distribution for each class. These scores will not
         # necessarily be well calibrated.
         print("\n**** Creating simulated data with Gaussian class distributions ****\n")
-
         np.random.seed(0)
 
         counts = [int(K * (1-prior1)), int(K * prior1)]
@@ -152,7 +170,9 @@ def get_llrs_for_bin_classif_task(dataset, prior1=None, K=100000):
         stds = [1.0, 1.0]
 
         # Generate random scores (raw log scaled-likelihoods) using the gaussian distribution for
-        # each class. The raw_scaled_lks have a 1 as the value for the first class.
+        # each class. The raw_scaled_lks have a 0 as the value for the first class since the
+        # method assumes the first lk is used as scale (ie, all lks are divided by that value). 
+        # The value for the second class is then the llr.
         raw_scaled_lks, targets = draw_scores_for_gaussian_model(means, stds, counts)
         raw_llrs = raw_scaled_lks[:,1]
 
@@ -170,15 +190,13 @@ def get_llrs_for_bin_classif_task(dataset, prior1=None, K=100000):
 
 
 def draw_scores_for_gaussian_model(means, stds, counts):
-    
-    """ Draw scores for each of C classes each with a Gaussian
-    distribution. The scores are meant to be (potentially)
+    """ 
+    Draw scores for C classes. The scores are meant to be (potentially)
     misscalibrated log scaled likelihoods. We assume the scale is
-    given by  the lk for the first class (any scale that is not class
-    dependent will work). Hence, the log scaled lks live in  a C-1
-    dimensional space. We assume that those C-1 dimensional vectors 
-    are Gaussian with diagonal covariance, with different means and 
-    stds for each class. 
+    given by the lk for the first class. Hence, the first component 
+    of the score vector is always 0 (log of 1). We assume that dimensions 
+    2 through C are Gaussian with diagonal covariance, with different 
+    means and stds for each class. 
 
     The means, stds and counts arguments are lists of size C
     containing the mean, std and number of samples for each class.
@@ -190,7 +208,7 @@ def draw_scores_for_gaussian_model(means, stds, counts):
         scores.append(multivariate_normal.rvs(mean, std, count))
         targets.append(np.ones(count)*i)
 
-    # Concatenate a column of 1s to the scores which corresponds to the
+    # Concatenate a column of 0s to the scores which corresponds to the
     # log scaled likelihood for the last class.
     scores = np.concatenate(scores)
     scores = np.c_[np.zeros(scores.shape[0]), scores]
