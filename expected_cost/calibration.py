5e9549c88447410d9cebd54f5a68851b8d9fe590
Luciana <lferrer@candombe.fibertel.com.ar>
2022-10-26 17:04:56 -0300

    Add functionality for calibration with external priors

--- expected_cost/calibration.py
+++ expected_cost/calibration.py
@@ -4,7 +4,7 @@ import torch
 import numpy as np
 
 
-def affine_calibration_with_crossval(logpost, targets, use_bias=True):
+def affine_calibration_with_crossval(logpost, targets, use_bias=True, priors=None):
     """ This is a utility method for performing calibration on the test scores
     using cross-validation. The calibration is done using an affine transformation
     of the form:
@@ -27,7 +27,7 @@ def affine_calibration_with_crossval(logpost, targets, use_bias=True):
         trnf = torch.as_tensor(logpost[trni], dtype=torch.float32)
         tstf = torch.as_tensor(logpost[tsti], dtype=torch.float32)
         trnt = torch.as_tensor(targets[trni], dtype=torch.int64)
-        tstf_cal, cal_params = calibrate(trnf, trnt, tstf, AffineCalLogLoss, bias=True)
+        tstf_cal, cal_params = calibrate(trnf, trnt, tstf, AffineCalLogLoss, bias=True, priors=priors)
         logpostcal[tsti] = tstf_cal.detach().numpy()
 
     return logpostcal
