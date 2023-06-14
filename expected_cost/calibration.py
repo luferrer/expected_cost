d9e0aad4a3d798ed9edf6d39fcf36f42baffdb05
Luciana Ferrer <lferrer@dc.uba.ar>
2023-06-14 18:24:03 -0300

    Add an option for no stratification and no conditions in the crossvalidation calibration method

--- expected_cost/calibration.py
+++ expected_cost/calibration.py
@@ -1,5 +1,5 @@
 from psrcal.calibration import AffineCalLogLoss
-from sklearn.model_selection import StratifiedKFold, StratifiedGroupKFold, GroupKFold
+from sklearn.model_selection import StratifiedKFold, StratifiedGroupKFold, GroupKFold, KFold
 import torch
 import numpy as np
 
@@ -46,7 +46,10 @@ def calibration_with_crossval(logpost, targets, calparams={}, calmethod=AffineCa
             # Use StratifiedKFold in this case for backward compatibility
             skf = StratifiedKFold(n_splits=nfolds, shuffle=True, random_state=seed)
     else:
-        skf = GroupKFold(n_splits=nfolds)
+        if condition_ids is not None:
+            skf = GroupKFold(n_splits=nfolds)
+        else:
+            skf = KFold(n_splits=nfolds, shuffle=True, random_state=seed)
 
     for trni, tsti in skf.split(logpost, targets, condition_ids):
 
